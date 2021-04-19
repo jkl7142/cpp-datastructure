558d284e34b57d9157db186e5d1802ea0a3a050c
KimJiHyeok <jkl7142@naver.com>
2021-04-19 17:23:17 +0900

    Use remove_if()

--- forward_list/forward_list.cpp
+++ forward_list/forward_list.cpp
@@ -35,5 +35,31 @@ int main(int argc, char* argv[]) {
     // forward_list.remove(val) : val와 값이 같은 원소를 모두 삭제
     // forward_list.remove_if() : 조건자 함수를 받아 true를 반환하는 원소를 모두 삭제
 
+    // 연결 리스트 정렬
+    // forward_list.sort() : 오름차순 정렬
+    // forward_list.sort(비교 함수) : 비교 함수 조건에 맞게 정렬
+    
+    std::forward_list<int> list1 = {23, 0, 1, -3, 34, 32};
+
+    list1.sort();   // {-3, 0, 1, 23, 32, 34}
+    list1.sort(std::greater<int>());    // {34, 32, 23, 1, 0, -3} 내림차순 정렬
+
+    // forward_list.reverse() : 역순으로 변경
+    // forward_list.unique() : 중복되는 원소에 대해 첫 번째만 남기고 나머지는 제거
+    // * unique()는 인접한 원소끼리 비교를 하기 때문에 정렬한 후 함수를 사용해야 함
+
+    std::forward_list<int> list2 = {2, 53, 1, 0, 4, 10};
+    list2.reverse();    // {10, 4, 0, 1, 53, 2}
+
+    list2 = {0, 1, 0, 1, -1, 10, 5, 5, 10, 0};
+    list2.unique(); // {0, 1, 0, 1, -1, 10, 5, 10, 0}
+
+    list2 = {0, 1, 0, 1, -1, 10, 5, 5, 10, 0};
+    list2.sort();   // {-1, 0, 0, 0, 1, 1, 5, 5, 10, 10}
+    list2.unique(); // {-1, 0, 1, 5, 10}
+
+    // 특정 원소가 바로 앞 원소보다 2 이상 크지 않으면 삭제
+    list2.unique([](int a, int b) { return (b - a) < 2; }); // {-1, 1, 5, 10}
+
     return 0;
 }
